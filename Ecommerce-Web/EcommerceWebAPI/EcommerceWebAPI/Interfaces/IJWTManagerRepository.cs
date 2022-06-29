eafc1c1397cc98edc8414662b81b33137a2ae089
KalyaniKhan <kalyanikhandetod97@gmail.com>
2022-06-29 12:31:46 +0530

    frist commit

---
+++ Ecommerce-Web/EcommerceWebAPI/EcommerceWebAPI/Interfaces/IJWTManagerRepository.cs
@@ -0,0 +1,14 @@
+﻿
+using EcommerceWebAPI.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EcommerceWebAPI.Interfaces
+{
+    public interface IJWTMangerRepository
+    {
+        Tokens Authenicate(LoginViewModel users, bool IsRegister);
+    }
+}
